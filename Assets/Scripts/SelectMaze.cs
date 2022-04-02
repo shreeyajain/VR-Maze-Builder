edefd6aa30423176222174d819d23d8369dcfea2
Shreeya Jain <shreeyaajain@gmail.com>
2022-04-02 00:31:46 -0400

    Implemented translation and collision prevention for selected maze

--- Assets/Scripts/SelectMaze.cs
+++ Assets/Scripts/SelectMaze.cs
@@ -1,6 +1,10 @@
+using System;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR;
 
 public class SelectMaze : MonoBehaviour
 {
@@ -16,13 +20,36 @@ public class SelectMaze : MonoBehaviour
     private bool wasBuildActive;
     private bool wasBuildingActive;
 
+    [SerializeField]
+    private XRNode xrNode = XRNode.RightHand;
+    private List<InputDevice> devices = new List<InputDevice>();
+    private InputDevice device;
+
+    private Vector3 target;
+    private Vector3 prevPos;
     private float targetY;
+    private bool canMove;
+
+    void GetDevice()
+    {
+        InputDevices.GetDevicesAtXRNode(xrNode, devices);
+        device = devices.FirstOrDefault();
+    }
+
+    void OnEnable()
+    {
+        if (!device.isValid)
+        {
+            GetDevice();
+        }
+    }
 
     void Awake()
     {
         select = false;
         wasBuildActive = false;
         wasBuildingActive = false;
+        canMove = true;
 
         canvas = GameObject.Find("Canvas");
         build = canvas.transform.GetChild(0).gameObject;
@@ -33,7 +60,67 @@ public class SelectMaze : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // Sanity check for getting device
+        if (!device.isValid)
+        {
+            GetDevice();
+        } 
+
+        Vector2 move = Vector2.zero;
+        if (select)
+        {
+            target = transform.position;
+            // Joystick movement to move the maze in x and z axes
+            if (device.TryGetFeatureValue(CommonUsages.primary2DAxis, out move) && move != Vector2.zero)
+            {
+                // Move in x direction
+                if (Mathf.Abs(move.x) > Mathf.Abs(move.y) && Mathf.Abs(move.x) > 0.6f && canMove)
+                {
+                    canMove = false;
+                    prevPos = transform.position;
+                    if (move.x > 0.0f)
+                    {
+                        target = transform.position + new Vector3(1, 0, 0);
+                    }
+                    else
+                    {
+                        target = transform.position - new Vector3(1, 0, 0);
+                    }
+                }
+                // Move in z direction
+                else if (Mathf.Abs(move.x) < Mathf.Abs(move.y) && Mathf.Abs(move.y) > 0.6f && canMove)
+                {
+                    canMove = false;
+                    prevPos = transform.position;
+                    if (move.y > 0.0f)
+                    {
+                        target = transform.position + new Vector3(0, 0, 1);
+                    }
+                    else
+                    {
+                        target = transform.position - new Vector3(0, 0, 1);
+                    }
+                }
+            }
+            else
+            {
+                canMove = true;
+            }
+
+            // If the object is being incorrectly placed 
+            // Conditions include being inside the 12x3x12 workspace boundary
+            // (x in [-5.5, 5.5], y in [0.5, 2.5], z in [-5.5, 5.5])
+            // Or if it's not currently colliding with any other object
+            if (target.x >= -5.5 && target.x <= 5.5 && target.y >= 0.5 && target.y <= 2.5 &&
+                    target.z >= -5.5 && target.z <= 5.5)
+            {
+                transform.position = target;
+            }
+            if (gameObject.GetComponent<CheckCollisionMaze>().anyCollision)
+            {
+                transform.position = prevPos;
+            }
+        }
     }
 
     public void Select()
